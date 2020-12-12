ae9b41d1ae90e8819cf6b95368d5a36628bb76c8
ddavid09 <ddawidziak.dd@gmail.com>
2020-12-12 14:36:35 +0100

    refacto handleItemClick func

--- Application/client-app/src/features/nav/SideBar.tsx
+++ Application/client-app/src/features/nav/SideBar.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { Header, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
+import { Icon, Menu, MenuItemProps, Sidebar } from "semantic-ui-react";
 
 const SideBar = () => {
   const [activeItem, setActiveItem] = useState<string | null>(null);
 
-  const handleItemClick = (name: string) => {
-    setActiveItem(name);
+  const handleItemClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    data: MenuItemProps
+  ) => {
+    if (data.name) {
+      setActiveItem(data.name);
+    }
   };
 
   return (
@@ -21,35 +26,19 @@ const SideBar = () => {
       width="thin"
       borderless
     >
-      <Menu.Item
-        name="home"
-        active={activeItem === "home"}
-        onClick={(e) => handleItemClick("home")}
-      >
+      <Menu.Item name="home" active={activeItem === "home"} onClick={handleItemClick}>
         <Icon name="home" />
         Strona główna
       </Menu.Item>
-      <Menu.Item
-        name="devices"
-        active={activeItem === "devices"}
-        onClick={(e) => handleItemClick("devices")}
-      >
+      <Menu.Item name="devices" active={activeItem === "devices"} onClick={handleItemClick}>
         <Icon name="hdd" />
         Urządzenia IoT
       </Menu.Item>
-      <Menu.Item
-        name="data"
-        active={activeItem === "data"}
-        onClick={(e) => handleItemClick("data")}
-      >
+      <Menu.Item name="data" active={activeItem === "data"} onClick={handleItemClick}>
         <Icon name="database" />
         Zgromadzone dane
       </Menu.Item>
-      <Menu.Item
-        name="analyse"
-        active={activeItem === "analyse"}
-        onClick={(e) => handleItemClick("analyse")}
-      >
+      <Menu.Item name="analyse" active={activeItem === "analyse"} onClick={handleItemClick}>
         <Icon name="chart bar" />
         Analiza danych
       </Menu.Item>
