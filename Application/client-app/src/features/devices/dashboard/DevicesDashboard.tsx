88d5193d4725439f7fbc7a6d29898c981aef46ac
ddavid09 <ddawidziak.dd@gmail.com>
2020-12-13 15:28:12 +0100

    all app loading indicator

--- Application/client-app/src/features/devices/dashboard/DevicesDashboard.tsx
+++ Application/client-app/src/features/devices/dashboard/DevicesDashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Button, Card, Grid, Icon, Image, Label } from "semantic-ui-react";
-import Devices from "../../../app/api/agent";
+import agent from "../../../app/api/agent";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { IDevice } from "../../../app/modules/device";
 import DeviceForm from "../form/DeviceForm";
 
@@ -8,25 +9,30 @@ const DevicesDashboard: React.FC = () => {
   const [devices, setDevices] = useState<IDevice[]>([]);
   const [editVisible, setEditVisible] = useState<boolean>(false);
   const [selectedDevice, setSelectedDevice] = useState<IDevice | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    Devices.getAll().then((response) => {
-      console.log("api getAll");
-      setDevices(response);
-    });
+    agent.Devices.getAll()
+      .then((response) => {
+        console.log("api getAll");
+        setDevices(response);
+      })
+      .then(() => setLoading(false));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  if (loading) return <LoadingComponent content="Ładowanie" />;
+
   const handleSubmit = (device: IDevice) => {
     if (selectedDevice) {
-      Devices.update(device).then(() => {
+      agent.Devices.update(device).then(() => {
         let devicesArray = [...devices];
         const index = devicesArray.findIndex((d) => d.deviceId === device.deviceId);
         devicesArray[index] = device;
         setDevices([...devicesArray]);
       });
     } else {
-      Devices.create(device).then(() => {
+      agent.Devices.create(device).then(() => {
         setDevices([...devices.filter((d) => d.deviceId !== device.deviceId), device]);
       });
     }
@@ -34,7 +40,7 @@ const DevicesDashboard: React.FC = () => {
   };
 
   const handleDelete = (device: IDevice) => {
-    Devices.delete(device.deviceId).then(() =>
+    agent.Devices.delete(device.deviceId).then(() =>
       setDevices([...devices.filter((d) => d.deviceId !== device.deviceId)])
     );
     if (selectedDevice && selectedDevice.deviceId === device.deviceId) {
