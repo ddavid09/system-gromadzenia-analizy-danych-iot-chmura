7b08d7546f26378a0041cd67358d612bcd0e06ae
ddavid09 <ddawidziak.dd@gmail.com>
2021-01-13 08:23:44 +0100

    main site azure architecture

--- Application/client-app/src/features/home/HomePage.tsx
+++ Application/client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 
 const HomePage = () => {
   return (
     <Segment basic>
-      <h1>Strona główna</h1>
+      <h1>
+        System gromadzenia i analizy danych sensorycznych z urządzeń IoT z wykorzystaniem
+        przetwarzania w chmurze
+      </h1>
+      <h3>
+        The system for collecting and analyzing sensory data from IoT devices using cloud computing
+      </h3>
+      <Image size="massive" src="assets/wysoki-poziom-azure.png" />
     </Segment>
   );
 };
